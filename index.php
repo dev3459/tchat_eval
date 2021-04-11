34aaf68c3332ca429893a8a6180676889351fe2f
dev3459 <test45@outlook.fr>
2021-04-11 21:45:42 +0200

    Changement de pc

---
+++ index.php
@@ -0,0 +1,34 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style_accueil.css">
+    <script src="https://kit.fontawesome.com/9e5ac608ee.js" crossorigin="anonymous"></script>
+    <title>Tchat Éval</title>
+</head>
+<body>
+    <div id="contains">
+        <div id="top">
+            <div id="title">
+                <h1>Tchat éval</h1>
+            </div>
+            <div id="registration">
+                <a href="" title="Connectez-vous pour pouvoir envoyé un message">Inscription / Connexion</a>
+            </div>
+        </div>
+        <div id="tchat">
+            <div id="message">
+                
+            </div>
+            <div id="writeTchat">
+                <form action="#" method="POST">
+                    <input type="text" name="message" placeholder="Écrivez un message ici" required>
+                    <button><i class="far fa-paper-plane"></i></button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
